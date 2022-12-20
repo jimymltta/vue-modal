f9bd57bc97537d5ea27b6288d4ce7e8e044600b2
Jimy Marletta <marlettajimy@gmail.com>
2022-12-20 18:16:58 +0100

    finished Vue modal exercise

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
+  <p>Welcome :)</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More infos</a>
+      </template>
+      <h1>Ninja Givaway!</h1>
+      <p>Grab your Ninja swag for half price!</p>
+    </Modal>
+  </teleport>
+
+  <div v-if="showModal2">
+    <Modal @close="toggleModal2">
+      <h1>Hello there!</h1>
+      <p>How's this second modal? Good, right?</p>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModal2">Open Modal 2</button>
 </template>
 
 <script>
@@ -12,13 +32,26 @@ export default {
   data() {
     return {
       title: 'My first Vue App',
+      header: 'Sign up for our giveaway!',
+      text: 'Grab your Ninja swag for half price!',
+      showModal: false,
+      showModal2: false,
     };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
   },
 };
 </script>
 
 <style>
-#app {
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
